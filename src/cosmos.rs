43acdfd0c4617db2c0a3be20f81731dac258a253
code0xff <ian.jungyong.um@gmail.com>
2022-10-23 21:42:19 +0900

    add hrp on cosmos address

--- src/cosmos.rs
+++ src/cosmos.rs
@@ -26,10 +26,10 @@ impl Address {
     })
   }
 
-  pub fn to_string(&self) -> Result<String, NoirError> {
+  pub fn to_string(&self, hrp: &str) -> Result<String, NoirError> {
     let data = self.inner.as_slice();
     let base32ed = data.to_base32();
-    let address = bech32::encode("cosmos", base32ed, Bech32)?;
+    let address = bech32::encode(hrp, base32ed, Bech32)?;
     Ok(address)
   }
 }
@@ -46,6 +46,19 @@ mod tests {
     let path = "m/44'/118'/0'/0/0".to_string();
     let derived = keypair.derive(&path).unwrap();
     let address = cosmos::Address::from_public(derived.public.as_bytes().as_ref()).unwrap();
-    assert_eq!(address.to_string().unwrap(), "cosmos1evlxjlzenjqvc6ddeur20ftrhp27fsv98jxhpr");
+    assert_eq!(address.to_string("cosmos").unwrap(), "cosmos1evlxjlzenjqvc6ddeur20ftrhp27fsv98jxhpr");
+  }
+
+  #[test]
+  fn hrp_address_test() {
+    let mnemonic = "buffalo squirrel angry will brain measure mechanic van adjust canyon theory burger".to_string();
+    let keypair = KeyPair::from_mnemonic(&mnemonic).unwrap();
+    let path = "m/44'/118'/0'/0/0".to_string();
+    let derived = keypair.derive(&path).unwrap();
+    let address = cosmos::Address::from_public(derived.public.as_bytes().as_ref()).unwrap();
+    assert_eq!(address.to_string("cosmos").unwrap(), "cosmos1hzuth7mwmaecq92r6kp4slfjvpk7a49j7s6n8e");
+    assert_eq!(address.to_string("osmo").unwrap(), "osmo1hzuth7mwmaecq92r6kp4slfjvpk7a49jktfr3t");
+    assert_eq!(address.to_string("juno").unwrap(), "juno1hzuth7mwmaecq92r6kp4slfjvpk7a49jgzegq9");
+    assert_eq!(address.to_string("stars").unwrap(), "stars1hzuth7mwmaecq92r6kp4slfjvpk7a49j2vdwvg");
   }
 }
